82a302218a092456270c4703048e5ee1666437e7
srikrishna0874 <kskvsubbarao2019@gmail.com>
2023-06-18 12:49:55 +0530

    Connected backend successfully

--- lib/db/constants.dart
+++ lib/db/constants.dart
@@ -1,3 +1,2 @@
-const MONGO_URL =
-    "mongodb+srv://srikrishna0874:Sri_12345@todo.bxkf5ib.mongodb.net/?retryWrites=true&w=majority";
-const COLLECTION_NAME1 = "UsersData";
+const MONGO_URL = "mongodb+srv://test:test@cluster0.xydlo25.mongodb.net/test?retryWrites=true&w=majority";
+const COLLECTION_NAME1 = "users";
--- lib/db/mongodb.dart
+++ lib/db/mongodb.dart
@@ -6,14 +6,13 @@ import 'constants.dart';
 
 class MongoDataBase {
   static connect() async {
-    print("\n\n\n\nHI\n\n\n\n\n");
+    print("\n\n\n\nConnection Started...\n\n\n\n\n");
     var db = await Db.create(MONGO_URL);
     await db.open();
     inspect(db);
-    var status = db.serverStatus();
-    print("\n\n\n\n\n\n${status}\n\n\n\n\n\n");
-    var collectionUsers = db.collection(COLLECTION_NAME1);
-
-    print(await collectionUsers.find().toList());
+    var status=db.serverStatus();
+    print(status);
+    var collection = db.collection(COLLECTION_NAME1);
+    print("Connection Successfully...");
   }
 }
